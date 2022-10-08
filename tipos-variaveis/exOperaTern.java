f2795e6eed3d10d9724a94d8115fb9c653290901
aguinaldo1 <aguinaldoamerico2@gmail.com>
2022-10-08 14:27:11 -0300

    subindo a classe exOperaTern

---
+++ tipos-variaveis/exOperaTern.java
@@ -0,0 +1,12 @@
+public class exOperaTern {
+    public static void main(String[] args) {
+        int a, b;
+        a = 6;
+        b = 6;
+
+        String resultado = a == b ? "verdadeiro" : "falso";// essa é uma expressão booleana
+
+        System.out.println(resultado);
+
+    }
+}
